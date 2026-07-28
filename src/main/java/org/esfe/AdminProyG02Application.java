650368c26bdb75a3012ef72c1fc332cce17e45b5
victor1206 <victor_rivas89@hotmail.com>
2026-07-28 09:55:21 -0600

    Primer commit

---
+++ src/main/java/org/esfe/AdminProyG02Application.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminProyG02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminProyG02Application.class, args);
+	}
+
+}
